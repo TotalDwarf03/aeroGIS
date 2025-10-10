bde294eafcf47aef6c4db0556c9210077443ef45
Kieran Pritchard <kierock03@gmail.com>
2025-10-10 21:54:31 +0100

    style: prettier fmt

--- src/setApiKey.js
+++ src/setApiKey.js
@@ -1,4 +1,4 @@
 // This file is ignored by Git to protect the API key
 // See the instructions in the README for more details
 
-apiKey = 'YOUR_API_KEY_HERE'; // Replace with your actual API key
+apiKey = "YOUR_API_KEY_HERE"; // Replace with your actual API key
