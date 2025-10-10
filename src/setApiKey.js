0561e87b9f78cf9b86a615a014785e876446dea9
Kieran Pritchard <kierock03@gmail.com>
2025-10-10 21:08:27 +0100

    chore: setApiKey comment update

--- src/setApiKey.js
+++ src/setApiKey.js
@@ -1,3 +1,4 @@
-// This file is included in the .gitignore to protect the API key
+// This file is ignored by Git to protect the API key
+// See the instructions in the README for more details
 
 apiKey = 'YOUR_API_KEY_HERE'; // Replace with your actual API key
