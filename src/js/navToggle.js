2e4611cb3c07b39d0c62c073dde82b2fb7a8d70c
Kieran Pritchard <kierock03@gmail.com>
2025-10-17 14:18:46 +0100

    style: prettier fmt

--- src/js/navToggle.js
+++ src/js/navToggle.js
@@ -1,8 +1,9 @@
 function navToggle() {
-    const navToggle = document.getElementById("navToggle");
-    const navLinks = document.getElementById("mobileNavLinks");
+  const navToggle = document.getElementById("navToggle");
+  const navLinks = document.getElementById("mobileNavLinks");
 
-    navLinks.classList.toggle("show");
-    navToggle.innerText = navLinks.classList.contains("show") ? "Collapse" : "Menu";
-
+  navLinks.classList.toggle("show");
+  navToggle.innerText = navLinks.classList.contains("show")
+    ? "Collapse"
+    : "Menu";
 }
--- src/js/themeSwitcher.js
+++ src/js/themeSwitcher.js
@@ -2,31 +2,31 @@ const darkModeIcon = "dark_mode";
 const lightModeIcon = "light_mode";
 
 const themeSwitcherIds = [
-    { buttonId: "themeSwitcherDesktop", iconId: "themeSwitcherIconDesktop" },
-    { buttonId: "themeSwitcherMobile", iconId: "themeSwitcherIconMobile" }
-]
+  { buttonId: "themeSwitcherDesktop", iconId: "themeSwitcherIconDesktop" },
+  { buttonId: "themeSwitcherMobile", iconId: "themeSwitcherIconMobile" },
+];
 
 function toggleTheme() {
-    const currentTheme = document.documentElement.getAttribute("data-theme");
-    const newTheme = currentTheme === "dark" ? "light" : "dark";
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-    document.documentElement.setAttribute("data-theme", newTheme);
+  document.documentElement.setAttribute("data-theme", newTheme);
 
-    themeSwitcherIds.forEach(({ buttonId, iconId }) => {
-        const themeSwitcherIcon = document.getElementById(iconId);
-        if (newTheme === "dark") {
-            themeSwitcherIcon.textContent = darkModeIcon;
-        } else {
-            themeSwitcherIcon.textContent = lightModeIcon;
-        }
-    });
+  themeSwitcherIds.forEach(({ buttonId, iconId }) => {
+    const themeSwitcherIcon = document.getElementById(iconId);
+    if (newTheme === "dark") {
+      themeSwitcherIcon.textContent = darkModeIcon;
+    } else {
+      themeSwitcherIcon.textContent = lightModeIcon;
+    }
+  });
 }
 
 // Initialize button text on page load
 document.addEventListener("DOMContentLoaded", () => {
-    const themeSwitcherButton = document.getElementById("themeSwitcherDesktop");
+  const themeSwitcherButton = document.getElementById("themeSwitcherDesktop");
 
-    // For some reason, the button doesn't reflect the theme until clicked once
-    // So we simulate a click to set it correctly - this is really stupid
-    themeSwitcherButton.click();
+  // For some reason, the button doesn't reflect the theme until clicked once
+  // So we simulate a click to set it correctly - this is really stupid
+  themeSwitcherButton.click();
 });
